59b7d8cb69e2996a7c16dbe541146f58a1a5befc
EliteSoba <tobiasle@usc.edu>
2012-11-26 00:35:52 -0800

    Fixed minor indentation

--- Email Spoofing/sendMail.py
+++ Email Spoofing/sendMail.py
@@ -39,7 +39,7 @@ f = " "
 
 #Minor redundancy in error checking
 if len(sys.argv) != 1:
-  for i in range(len(sys.argv)):
+	for i in range(len(sys.argv)):
 		if i == 0:
 			continue
 		command = sys.argv[i]
